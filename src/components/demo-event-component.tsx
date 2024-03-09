2655f5e7483b47553f435eb0b0400907ee56dec4
galina101 <goncharovagalinka@gmail.com>
2024-03-09 14:47:47 -0500

    saving lab progress: 03-09 14:47

--- src/components/demo-event-component.tsx
+++ src/components/demo-event-component.tsx
@@ -15,9 +15,12 @@ const DemoEventComponent: React.FC<MyProps> = (props: MyProps) => {
         //you can put whatever complex logic you want in here.
 
         /*
-            SythenticEvent is a generic type of Event object that React gives you; you don't have to include the event parameter, but if you need
-            metadata regarding then opting into the event object may be a good idea. If you need more specific metadata about the event then you
-            may want to choose a more specific event datatype; there are lots of them, for example, MouseEent, KeyboardEvent, FormEvent, ChangeEvent, etc.
+            SythenticEvent is a generic type of Event object that React gives you;
+            you don't have to include the event parameter, but if you need
+            metadata regarding then opting into the event object may be a good idea.
+            If you need more specific metadata about the event then you
+            may want to choose a more specific event datatype; there are lots of them,
+            for example, MouseEvent, KeyboardEvent, FormEvent, ChangeEvent, etc.
         */
 
 
@@ -36,7 +39,8 @@ const DemoEventComponent: React.FC<MyProps> = (props: MyProps) => {
             <p>This button will cause Demo Value to count up until it hits 3, then it will loop the value back around to -3, repeat.</p>
             <p>Demo Value: {demoNumberState}</p>
 
-            {/* Notice how the function doesn't have parenthesis at the end, you're referencing a function, not invoking it. */}
+            {/* Notice how the function doesn't have parenthesis at the end,
+            you're referencing a function, not invoking it. */}
             <button onClick={myEventFunction}>Demo Event Button</button>
 
         </React.Fragment>
--- src/components/todo-event-component.tsx
+++ src/components/todo-event-component.tsx
@@ -10,19 +10,25 @@ const TodoEventComponent: React.FC<MyProps> = (props: MyProps) => {
     //Creating some state for the component
     const [todoNumberState, setTodoNumberState] = useState(0);
 
-
+    const myEventFunction = (synthEvent: SyntheticEvent) => {
+        if (todoNumberState == 0)
+            setTodoNumberState(todoNumberState + 1);
+        else if (todoNumberState > 0)
+            setTodoNumberState(todoNumberState*2);
+    }
 
     return (
         <React.Fragment>
             <h4>Todo Event Component</h4>
 
             {/* TODO */}
-            <p>TODO: create an event on the button that alters the value of "todoNumberState". If the value of todoNumberState is 0 then the button
+            <p>TODO: create an event on the button that alters the value of "todoNumberState".
+                If the value of todoNumberState is 0 then the button
             should add 1; if the value is greater than 0 then the button should double the value of todoNumberState.</p>
 
             <p>Todo Value: {todoNumberState}</p>
             
-            <button >Todo Event Button</button>
+            <button onClick={myEventFunction}>Todo Event Button</button>
 
         </React.Fragment>
     );
